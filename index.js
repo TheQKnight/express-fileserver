e558620bf2ecfbf6fe6f9712b84f6005af992b67
Elijah Lipsky <theqknight@gmail.com>
2021-02-08 23:30:19 -0800

    Added config options for ports

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path')
 
 const app = express()
 
+const config = JSON.parse(
+  fs.readFileSync(path.join(__dirname, './config.json'))
+)
+
 app.use('/', express.static(path.join(__dirname, './public')))
 
 app.get('*', (req, res) => {
@@ -15,10 +19,10 @@ app.get('*', (req, res) => {
 try {
   const sslKey = fs.readFileSync(path.join(__dirname, './ssl/key.pem'))
   const sslCert = fs.readFileSync(path.join(__dirname, './ssl/cert.pem'))
-  https.createServer({ key: sslKey, cert: sslCert }, app).listen(443)
+  https.createServer({ key: sslKey, cert: sslCert }, app).listen(config.https)
   console.log('HTTPS server listening on port 443')
 } catch (err) {
   console.log('Could not initialize HTTPS server. Using HTTP only.')
 }
-http.createServer(app).listen(80)
+http.createServer(app).listen(config.http)
 console.log('HTTP server listening on port 80')
